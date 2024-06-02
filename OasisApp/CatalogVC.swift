7aaf94853b56dbba7b0a0290430302f0bdbc2e67
Pedro Flores <pedro.r.flores13@gmail.com>
2024-06-02 09:56:48 -0600

    Primeras 5 interfaces listas

---
+++ OasisApp/CatalogVC.swift
@@ -0,0 +1,26 @@
+//
+//  CatalogVC.swift
+//  OasisApp
+//
+//  Created by Pedro de Jesús Razo Flores on 02/06/24.
+//
+
+import UIKit
+
+class CatalogVC: UIViewController {
+    
+    
+//    @IBOutlet weak var btnView: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        btnView.backgroundColor = UIColor.clear
+//        btnView.layer.borderColor = UIColor.white.cgColor
+//        btnView.layer.borderWidth = 2
+//        btnView.layer.cornerRadius = 2
+
+    }
+    
+
+}
