0a021f972ec6fc0cc299fdcebcb3589f5755d3d5
Pedro Flores <pedro.r.flores13@gmail.com>
2024-06-04 20:18:07 -0600

    Servicio de Login listo

--- OasisApp-Server/src/java/org/team/restoasis/model/User.java
+++ OasisApp-Server/src/java/org/team/restoasis/model/User.java
@@ -10,4 +10,73 @@ package org.team.restoasis.model;
  */
 public class User {
     
+    private int user_id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String addres;
+    private String phone;
+    
+    public User(){
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    
+    
 }
--- OasisApp-Server/src/java/org/team/restoasis/rest/RestAuth.java
+++ OasisApp-Server/src/java/org/team/restoasis/rest/RestAuth.java
@@ -4,10 +4,50 @@
  */
 package org.team.restoasis.rest;
 
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.team.restoasis.controller.ControllerAuth;
+import org.team.restoasis.model.User;
+
 /**
  *
  * @author aj898
  */
+@Path("user")
 public class RestAuth {
+    
+    @POST
+@Produces(MediaType.APPLICATION_JSON)
+@Path("login")
+public Response login(@FormParam("username") String username,
+                      @FormParam("password") String password) {
+    String out = "";
+    try {
+        ControllerAuth cu = new ControllerAuth();
+        User user = cu.getByUsernameAndPassword(username, password);
+        if (user != null) {
+            System.out.println(username);
+            out = """
+                 {"user_id": "%d"}
+                 """.formatted(user.getUser_id());
+        } else {
+            out = """
+                 {"user_id":"0"}
+                 """;
+        }
+    } catch (Exception e) {
+        out = """
+             {"error":"%s"}
+             """;
+        out = String.format(out, e.getMessage());
+    }
+    return Response.ok(out).build();
+}
+    
+    
     
 }
