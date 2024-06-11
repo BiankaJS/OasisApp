1f9704e3d8a62df9c672559f7c532ef7f9d300f2
Bianka <biankasarayj@outlook.com>
2024-06-11 11:53:06 -0600

    Modifed login

--- OasisApp-Server/src/java/org/team/restoasis/rest/RestAuth.java
+++ OasisApp-Server/src/java/org/team/restoasis/rest/RestAuth.java
@@ -5,9 +5,11 @@
 package org.team.restoasis.rest;
 
 import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.team.restoasis.controller.ControllerAuth;
@@ -17,30 +19,28 @@ import org.team.restoasis.model.User;
  *
  * @author aj898
  */
-@Path("user")
+@Path("auth")
 public class RestAuth {
-
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("login")
-    public Response login(@FormParam("username") String username,
-            @FormParam("password") String password) {
-        String out = "";
-        try {
-            ControllerAuth cu = new ControllerAuth();
-            User user = cu.getByUsernameAndPassword(username, password);
-            if (user != null) {
-                System.out.println(username);
-                out = """
+    
+@POST
+@Produces(MediaType.APPLICATION_JSON)
+@Path("login")
+public Response login(@QueryParam("username") String username,
+                      @QueryParam("password") String password) {
+    String out = "";
+    try {
+        ControllerAuth cu = new ControllerAuth();
+        User user = cu.getByUsernameAndPassword(username, password);
+        if (user != null) {
+            System.out.println(username);
+            out = """
                  {"user_id": "%d"}
                  """.formatted(user.getUser_id());
-            } else {
-                out = """
-                 {"user_id":"0"}
-                 """;
-            }
-        } catch (Exception e) {
-            out = """
+        } else {
+            return Response.status(401).build();
+        }
+    } catch (Exception e) {
+        out = """
              {"error":"%s"}
              """;
             out = String.format(out, e.getMessage());
